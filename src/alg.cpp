4dbec7580d46f38a9b4ff841b9886597e113de9e
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-06-03 14:40:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,19 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+    BST<std::string> tree;
+  std::ifstream file(filename);
+  std::string word;
+  while (!file.eof()) {
+    char c = file.get();
+    if (c >= 'A' && c <= 'Z')
+        c = c + ('a' - 'A');
+    if (c >= 'a' && c <= 'z') {
+      word += c;
+    } else {
+      tree.add(word);
+      word.clear();
+    }
+  }
+  return tree;
 }
